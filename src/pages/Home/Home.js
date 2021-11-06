843bc37bcf73ac07bfb94df3fd8a8176ed0de2ac
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-06 23:53:25 +0600

    asdf

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import Banner from '../../components/Banner/Banner';
 import NavBar from '../../components/NavBar/NavBar';
 import Services from '../../components/Services/Services';
@@ -9,6 +10,20 @@ const Home = () => {
             <NavBar></NavBar>
             <Banner></Banner>
             <Services></Services>
+
+
+            <Container>
+                <Row>
+                    <div>
+                        <Col md={6}>
+                            
+                        </Col>
+                        <Col md={6}>
+                            
+                        </Col>
+                    </div>
+                </Row>
+            </Container>
         </>
     );
 };
