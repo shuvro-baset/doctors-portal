035cbc06b894885ba9dc5bf7ab70e2c2fc2da502
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-06 21:31:31 +0600

    banner done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,6 +12,7 @@ const Banner = () => {
                     <div className="banner d-flex flex-column justify-content-center ">
                         <h2>Your New Smile <br /> Starts Here</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quisquam soluta odit distinctio, unde dolor voluptatibus libero autem reiciendis eum.</p>
+                        <button className="custom-btn">Get Appointment</button>
                     </div>
                 </Col>
                 <Col md={6}>
@@ -19,6 +20,36 @@ const Banner = () => {
                         <img className="img-fluid" src={bannerImg} alt="" />
                     </div>
                 </Col>
+            </Row>
+            <Row className="my-5">
+                <Col md={4}>
+                    <div className="py-3 d-flex justify-content-evenly align-items-center banner-div">
+                        <i className="far fa-clock banner-icon"></i>
+                        <div>
+                            <h4> Opening Hours</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                    <div className="py-3 d-flex justify-content-evenly align-items-center banner-div2">
+                    <i className="fas fa-map-marker-alt banner-icon"></i>
+                        <div>
+                            <h4>Visit Our Location</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                    <div className="py-3 d-flex justify-content-evenly align-items-center banner-div">
+                        <i className="fas fa-phone-alt banner-icon"></i>
+                        <div>
+                            <h4>Contact Us Now</h4>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
+                </Col>
+
             </Row>
         </Container>
     );
