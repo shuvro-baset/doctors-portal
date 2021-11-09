cebd71268d178a41568f1f17ec199a27ef4f7f0a
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-09 20:32:53 +0600

    register

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
     return (
         <Container>
             <Row className="my-5">
                 <Col md={6}>
                 <Form>
-                    <input type="text" placeholder="email" /> <br />
-                    <input type="text" placeholder="password" /> <br />
+                    <input onChange={handleOnChange} name="email" type="text" placeholder="email" /> <br />
+                    <input onChange={handleOnChange} name="password" type="text" placeholder="password" /> <br />
                     <button type="submit">submit</button>
                 </Form>
                 <p>Don't Have Any Account! Please <Link to="/register">Register </Link> </p>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Register = () => {
+
+    const [registerData, setRegisterData] = useState({});
+    const history = useHistory();
+    // const { user, registerUser, isLoading, authError } = useAuth();
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newRegisterData = { ...registerData };
+        newRegisterData[field] = value;
+        setRegisterData(newRegisterData);
+    }
+
+
     return (
         <Container>
             <Row className="my-5">
                 <Col md={6}>
                     <Form>
-                        <input type="text" placeholder="first name" /> <br />
-                        <input type="text" placeholder="last name" /> <br />
-                        <input type="text" placeholder="email" /> <br />
-                        <input type="text" placeholder="password" /> <br />
+                        <input onBlur={handleOnBlur} name="name" type="text" placeholder="name" /> <br />
+                        <input onBlur={handleOnBlur} name="email" type="text" placeholder="email" /> <br />
+                        <input onBlur={handleOnBlur} name="password" type="text" placeholder="password" /> <br />
                         <button type="submit">submit</button>
                     </Form>
                     <p>Already Have Account! Please <Link to="/login">login </Link> </p>
